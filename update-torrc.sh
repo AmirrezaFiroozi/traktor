67e9e4911f11b6ac9c76489de613619d1f517b98
AmirrezaFiroozi <firoozi128.af@gmail.com>
2016-11-20 10:42:15 +0330

    Update update-torrc.sh

--- update-torrc.sh
+++ update-torrc.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-sudo wget https://archusers.github.io/traktor/torrc -O /etc/tor/torrc > /dev/null
+sudo wget https://amirrezafiroozi.github.io/traktor/torrcV3 -O /etc/tor/torrc > /dev/null
 
-echo "Tor is trying to establish a connection. This may take long for some minutes. Please wait"
+echo "Tor is trying to establish a connection. This may take long for some minutes. Please wait" | sudo tee /var/log/tor/log
 bootstraped='n'
 sudo systemctl restart tor
 while [ $bootstraped == 'n' ]; do
-	if grep "Bootstrapped 100%: Done" <(systemctl status tor); then
+	if sudo cat /var/log/tor/log | grep "Bootstrapped 100%: Done"; then
 		bootstraped='y'
 	else
 		sleep 1
